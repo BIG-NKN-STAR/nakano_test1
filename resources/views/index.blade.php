edfca4787c0dda0ac8d034be3e6b810bb0b5b3c8
BIG-NKN-STAR <jin.0205.nakano@gmail.com>
2022-03-23 16:27:08 +0900

    [add]create追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,16 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-       
         <title>blog</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
     </head>
     <body>
        <h1>Blog Name</h1>
-       <div class-'posts'>
+       <p class='create'>[<a href='/posts/create'>create</a>]</p>
+       <div class='posts'>
            @foreach ($posts as $post)
                <div class='post'>
                    <a href='/posts/{{ $post->id }}'><h2 class='title'>{{ $post->title }}</h2></a>
